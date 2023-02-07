e09220024bd852b1778c45cc125cde9a2dcd2b52
Fullangel <99usui99@gmail.com>
2023-02-06 22:28:59 -0400

    Added cart and context

--- context/StateContext.js
+++ context/StateContext.js
@@ -37,11 +37,20 @@ export const StateContext = ({ children }) => {
         toast.success(`${qty} ${product.name} added to cart.`);
     }
 
-    const newCartItems = cartItems.filter((item) => item._id !== id);
+    const onRemove = (product) => {
+        foundProduct = cartItems.find((item) => item._id === product._id);
+        const newCartItems = cartItems.filter((item) => item._id !== product._id);
+
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - foundProduct.quantity);
+        setCartItems(newCartItems);
+    }
+
 
     const toggleCartItemQuantity = (id, value) => {
         foundProduct = cartItems.find((item) => item._id === id);
         index = cartItems.findIndex((product) => product._id === id);
+        const newCartItems = cartItems.filter((item) => item._id !== id);
 
         if(value === 'inc'){
             setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 } ]);
@@ -80,7 +89,8 @@ export const StateContext = ({ children }) => {
         incQty,
         decQty,
         onAdd,
-        toggleCartItemQuantity
+        toggleCartItemQuantity,
+        onRemove
     }}
     >
         {children}
